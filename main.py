1802d8ddaf0affd6e1ce2e2eb6367af0b8d646d2
Константин <zverevka@yandexlyceum.ru>
2024-02-01 16:42:32 +0300

    Капучино (открытие окна)

--- main.py
+++ main.py
@@ -2,31 +2,40 @@ import sys
 import sqlite3
 from PyQt5 import uic
 from PyQt5.QtWidgets import QTableWidgetItem
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 
 RU_ALF = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЭЮЯ"
 
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.tableWidget.setHorizontalHeaderLabels(['ID', 'название сорта', 'степень обжарки',
                                                     'вид помола', 'описание вкуса', 'цена', 'объем упаковки'])
-        self.show_button.clicked.connect(self.btn_click)
+        self.show_button.clicked.connect(self.refresh_list)
+        self.add_button.clicked.connect(self.show_addform)
 
-    def btn_click(self):
+    def show_addform(self):
+        add_window.show()
+
+    def refresh_list(self):
         con = sqlite3.connect("coffee.sqlite")
         cur = con.cursor()
         result = cur.execute(f"""SELECT * from coffee_table""").fetchall() #вывод всего из БД
         con.close()
-        self.tableWidget.setRowCount(0)
         for row_data in result:
             row_number = self.tableWidget.rowCount()
             self.tableWidget.insertRow(row_number)
             for column_number, data in enumerate(row_data):
                 self.tableWidget.setItem(
                     row_number, column_number, QTableWidgetItem(str(data)))
+
+
+class AddWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
 
 
 if __name__ == '__main__':
@@ -39,7 +48,8 @@ if __name__ == '__main__':
         QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = MainWindow()
+    add_window = AddWindow()
     ex.show()
     sys.exit(app.exec())
 
